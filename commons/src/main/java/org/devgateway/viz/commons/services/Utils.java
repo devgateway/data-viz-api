b7c6e3ce5c6fda14a40da16d7ad6bf67406c5962
mbertoli <mfbertoli@gmail.com>
2024-01-05 17:17:54 -0300

    CNFA-1241

--- commons/src/main/java/org/devgateway/viz/commons/services/Utils.java
+++ commons/src/main/java/org/devgateway/viz/commons/services/Utils.java
@@ -1,9 +1,33 @@
 package org.devgateway.viz.commons.services;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Utils {
 
     public static String codify(String label) {
         return label.trim().toUpperCase().replaceAll(" ", "_").replaceAll("[^a-zA-Z0-9_]", "");
+    }
+
+    public static List parseParams(String value, Class type) {
+        String[] strValues = value.split(",");
+        return Arrays.stream(strValues).map(s -> {
+            try {
+                return type.getConstructor(String.class).newInstance(s);
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }).collect(Collectors.toList());
+
     }
 
 }
