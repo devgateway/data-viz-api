79f136f938f6b5d61fac792861fcf18866f17809
ginchauspe <ginchauspe@gmail.com>
2024-05-14 15:36:40 -0300

    CNFA-1342: Add annotation to translate dimensions.

--- commons/src/main/java/org/devgateway/viz/commons/domain/annotations/Dimension.java
+++ commons/src/main/java/org/devgateway/viz/commons/domain/annotations/Dimension.java
@@ -12,4 +12,5 @@ import java.lang.annotation.Target;
 public @interface Dimension {
     String label()  default "[unassigned]";
     Class type() default Class.class;
+    Translation[] translations() default {};
 }
