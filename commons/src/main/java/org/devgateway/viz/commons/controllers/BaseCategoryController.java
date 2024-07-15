67752a724b66aa4e5d7981869967eb56df825460
Hush <allannelly690@gmail.com>
2024-07-15 12:32:59 +0300

    update base category controller

--- commons/src/main/java/org/devgateway/viz/commons/controllers/BaseCategoryController.java
+++ commons/src/main/java/org/devgateway/viz/commons/controllers/BaseCategoryController.java
@@ -3,6 +3,9 @@ package org.devgateway.viz.commons.controllers;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RequestMapping(produces = APPLICATION_JSON_VALUE)
 @RestController
 @CrossOrigin("*")
 public class BaseCategoryController {
