029dd1faa464414a2881053be19d3e9008682fce
Hush <allannelly690@gmail.com>
2024-07-16 13:31:29 +0300

    fix missing symbols

--- commons/src/main/java/org/devgateway/viz/commons/controllers/BaseCategoryController.java
+++ commons/src/main/java/org/devgateway/viz/commons/controllers/BaseCategoryController.java
@@ -1,6 +1,6 @@
 package org.devgateway.viz.commons.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
