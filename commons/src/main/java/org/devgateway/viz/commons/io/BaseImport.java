64e3472fb6446935c8f98466f7fd457e438261a7
Denis Mbugua <60645043+dynamodenis@users.noreply.github.com>
2024-09-20 22:54:31 +0300

    Updating code changes

--- commons/src/main/java/org/devgateway/viz/commons/io/BaseImport.java
+++ commons/src/main/java/org/devgateway/viz/commons/io/BaseImport.java
@@ -1,15 +1,13 @@
 package org.devgateway.viz.commons.io;
 
-import liquibase.pro.packaged.T;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import org.apache.commons.lang.StringUtils;
 import org.devgateway.viz.commons.services.CategoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 
 @Transactional
 public abstract class BaseImport<T, R> {
