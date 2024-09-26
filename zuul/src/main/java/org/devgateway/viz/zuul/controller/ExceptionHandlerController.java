187dcb3be3541aa23f363a54f2a1b74207404415
Denis Mbugua <60645043+dynamodenis@users.noreply.github.com>
2024-09-26 14:31:33 +0300

    DVIZ-11 upgraded springboot to 3.2.10

--- zuul/src/main/java/org/devgateway/viz/zuul/controller/ExceptionHandlerController.java
+++ zuul/src/main/java/org/devgateway/viz/zuul/controller/ExceptionHandlerController.java
@@ -4,7 +4,7 @@ import org.devgateway.viz.zuul.security.exception.AuthException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ExceptionHandlerController {
