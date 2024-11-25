17c2361d82a55b49f768314c72ced9712c16c966
Denis Mbugua <60645043+dynamodenis@users.noreply.github.com>
2024-11-25 16:09:57 +0300

    Added gateway dependency

--- zuul/src/main/java/org/devgateway/viz/zuul/Main.java
+++ zuul/src/main/java/org/devgateway/viz/zuul/Main.java
@@ -3,14 +3,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
 import java.util.Collections;
 
 @SpringBootApplication
-@EnableZuulProxy
+@EnableGateway
 @EnableDiscoveryClient
 public class Main {
 
