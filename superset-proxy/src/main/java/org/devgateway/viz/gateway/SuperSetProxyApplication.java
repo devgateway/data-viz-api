05646e39a573e256ebf4e01c5722c84e173bd17f
Sebastian Dimunzio <sdimunzio@gmail.com>
2025-01-09 15:54:56 -0300

    adding proxy skeleton generated with GPT from the original draft node code

--- superset-proxy/src/main/java/org/devgateway/viz/gateway/SuperSetProxyApplication.java
+++ superset-proxy/src/main/java/org/devgateway/viz/gateway/SuperSetProxyApplication.java
@@ -3,12 +3,15 @@ package org.devgateway.viz.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+
+
 public class SuperSetProxyApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SuperSetProxyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SuperSetProxyApplication.class, args);
+    }
 
 }
