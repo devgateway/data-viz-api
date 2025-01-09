18553168087112b1f6ed905a4843ef469aa7c97a
Sebastian Dimunzio <sdimunzio@gmail.com>
2025-01-09 16:00:30 -0300

    adding proxy skeleton generated with GPT from the original draft node code

--- superset-proxy/src/main/java/org/devgateway/viz/gateway/controllers/SupersetController.java
+++ superset-proxy/src/main/java/org/devgateway/viz/gateway/controllers/SupersetController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/api/alive-superset")
+@RequestMapping("/")
 @CrossOrigin(origins = "*") // Allow all origins
 public class SupersetController {
 
