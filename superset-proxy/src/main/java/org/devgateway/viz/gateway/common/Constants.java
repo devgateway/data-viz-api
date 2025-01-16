0048519adc1597f3ae8fafe116d26f7a608a5427
gmutuhu <geraldmutuhu@gmail.com>
2025-01-16 10:21:49 +0300

    ALDASH-77 enable categories and stats caching
    ALDASH-77 enable categories and stats caching

---
+++ superset-proxy/src/main/java/org/devgateway/viz/gateway/common/Constants.java
@@ -0,0 +1,13 @@
+package org.devgateway.viz.gateway.common;
+
+import java.util.List;
+
+public class Constants {
+    public static final Long ROW_LIMIT = Long.MAX_VALUE;
+
+    public static final List<String> SPECIAL_PARAMS = List.of("datasetId", "row_limit");
+
+    public static final List<String> COLORS = List.of(
+            "***REMOVED***1f77b4", "***REMOVED***ff7f0e", "***REMOVED***2ca02c", "***REMOVED***d62728", "***REMOVED***9467bd", "***REMOVED***8c564b", "***REMOVED***e377c2", "***REMOVED***7f7f7f", "***REMOVED***bcbd22", "***REMOVED***17becf"
+    );
+}
