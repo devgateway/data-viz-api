b8a086b51477cb5aed69f9fdea1594e56dbda8be
gmutuhu <geraldmutuhu@gmail.com>
2025-01-16 13:20:58 +0300

    ALDASH-77 superset proxy changes
    ALDASH-77 superset proxy changes

--- superset-proxy/src/main/java/org/devgateway/viz/gateway/common/Constants.java
+++ superset-proxy/src/main/java/org/devgateway/viz/gateway/common/Constants.java
@@ -7,6 +7,10 @@ public class Constants {
 
     public static final List<String> SPECIAL_PARAMS = List.of("datasetId", "row_limit");
 
+    public static final String MEASURE_GROUP_LABEL = "Summary Statistics";
+
+    public static final String DEFAULT_COLOR = "#555";
+
     public static final List<String> COLORS = List.of(
             "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"
     );
