0071035cb429cac7dd60250d67a2465a8d899ae9
Denis Mbugua <60645043+dynamodenis@users.noreply.github.com>
2025-01-23 19:43:16 +0300

    DVIZ-17 merged tcdi-official-main with master

--- commons/src/main/java/org/devgateway/viz/commons/controllers/GenericCategoryController.java
+++ commons/src/main/java/org/devgateway/viz/commons/controllers/GenericCategoryController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.Collection;
 import java.util.Map;
 
