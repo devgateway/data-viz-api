9b95190ba9de3c1cfceb32519e6ef68410d520d1
Sebastian Dimunzio <sdimunzio@gmail.com>
2025-03-13 12:29:45 -0300

    ALDASH-161 - adding cache

--- api-gateway/src/main/java/org/devgateway/viz/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/org/devgateway/viz/gateway/ApiGatewayApplication.java
@@ -1,14 +1,30 @@
 package org.devgateway.viz.gateway;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 
+
+    @Bean
+    public Caffeine caffeineConfig() {
+        return Caffeine.newBuilder().expireAfterWrite(60, java.util.concurrent.TimeUnit.MINUTES);
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine caffeine) {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
+    }
 }
