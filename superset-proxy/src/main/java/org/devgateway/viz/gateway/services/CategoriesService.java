36885f4089f68487228dad82ee3d857f17c8b2f3
Sebastian Dimunzio <sdimunzio@gmail.com>
2025-05-07 16:15:30 -0300

    ALDASH-260 - Adding logger to track empty responses

--- superset-proxy/src/main/java/org/devgateway/viz/gateway/services/CategoriesService.java
+++ superset-proxy/src/main/java/org/devgateway/viz/gateway/services/CategoriesService.java
@@ -8,7 +8,6 @@ import org.devgateway.viz.gateway.services.rest.SuperSetClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -32,7 +31,7 @@ public class CategoriesService {
 
     }
 
-    //@Cacheable(value = "categories", key = "#datasetId")
+
     public List<Map<String, Object>> getCategories(String datasetId) {
         if (datasetId == null) {
             return Collections.emptyList();
