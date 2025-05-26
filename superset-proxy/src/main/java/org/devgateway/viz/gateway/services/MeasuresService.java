a3ae0f15de2db650fc76bafb30adf4c158799fd4
Sebastian Dimunzio <sdimunzio@gmail.com>
2025-05-26 17:21:26 -0300

    putting back local cache

--- superset-proxy/src/main/java/org/devgateway/viz/gateway/services/MeasuresService.java
+++ superset-proxy/src/main/java/org/devgateway/viz/gateway/services/MeasuresService.java
@@ -7,6 +7,7 @@ import org.devgateway.viz.gateway.services.rest.SuperSetClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -30,6 +31,7 @@ public class MeasuresService {
         this.objectMapper = objectMapper;
     }
 
+    @Cacheable(value = "measures", key = "#datasetId")
     public List<Map<String, Object>> getMeasures(String datasetId) {
         if (datasetId == null) {
             return Collections.emptyList();
