79f30a79cc826c58d52bd313e5769292902d444d
Octavian Ciubotaru <ociubotaru@developmentgateway.org>
2025-05-29 11:20:13 +0300

    ALDASH-285: Changes to caching

--- superset-proxy/src/main/java/org/devgateway/viz/gateway/services/CategoriesService.java
+++ superset-proxy/src/main/java/org/devgateway/viz/gateway/services/CategoriesService.java
@@ -1,6 +1,5 @@
 package org.devgateway.viz.gateway.services;
 
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.devgateway.viz.gateway.common.Constants;
@@ -8,7 +7,6 @@ import org.devgateway.viz.gateway.services.rest.SuperSetClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -34,7 +32,6 @@ public class CategoriesService {
     }
 
 
-    @Cacheable(value = "categories", key = "#datasetId")
     public List<Map<String, Object>> getCategories(String datasetId) {
         if (datasetId == null) {
             return Collections.emptyList();
